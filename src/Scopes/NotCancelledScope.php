4d14b46694539d04c9df5d38db80ca893f10df25
Firose Hussain <github@firumon.com>
2020-10-07 12:37:07 +0530

    Not cancelled scope added

---
+++ src/Scopes/NotCancelledScope.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Milestone\SmartKitchen\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class NotCancelledScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('progress','!=','Cancelled');
+    }
+
+}
