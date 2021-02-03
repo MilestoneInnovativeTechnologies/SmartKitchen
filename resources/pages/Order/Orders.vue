eaf785a976f0694e0b7b05f8e5c1ac473bda2338
Firose Hussain <github@firumon.com>
2021-02-03 16:31:32 +0530

    Ordering fab for waiter corrected

--- resources/pages/Order/Orders.vue
+++ resources/pages/Order/Orders.vue
@@ -1,13 +1,15 @@
 <template>
   <q-page padding v-scroll="scrolled">
     <div class="row q-col-gutter-xs items-start">
-      <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-1" v-for="(token,id) in tokens" :key="hKey(token)">
-        <OrderSummaryWaiterItem :id="id" />
+      <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-1" v-for="token in tokens" :key="hKey(token)">
+        <OrderSummaryWaiterItem :id="token.id" />
       </div>
     </div>
-    <q-page-sticky v-show="fab || !Object.keys(tokens).length" position="bottom-right" :offset="offset">
-      <q-fab icon="add" color="primary" glossy :to="{ name: 'order_new' }" v-touch-pan.prevent.mouse="move" />
-    </q-page-sticky>
+    <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+      <q-page-sticky v-show="fab || !tokens.length" position="bottom-right" :offset="offset">
+        <q-fab icon="add" color="primary" glossy :to="{ name: 'order_new' }" v-touch-pan.prevent.mouse="move" />
+      </q-page-sticky>
+    </transition>
   </q-page>
 </template>
 
@@ -15,6 +17,7 @@
 import OrderSummaryWaiterItem from "components/Order/OrderSummaryWaiterOrder";
 import {h_key} from "assets/helpers";
 import {mapState} from "vuex";
+import { debounce } from 'quasar'
 export default {
   name: 'PageOrders',
   components: {OrderSummaryWaiterItem},
@@ -23,17 +26,17 @@ export default {
     progresses: ['New','Processing','Completed']
   } },
   computed: mapState('tokens',{
-    tokens({ data }){ return _(data).filter(({ progress }) => this.progresses.includes(progress)).mapKeys('id').value() }
+    tokens({ data }){ return _(data).filter(({ progress }) => this.progresses.includes(progress)).value() }
   }),
   methods: {
     hKey({ id }){ h_key('order','summary','order',id) },
-    scrolled(pos){ this.fab = true },
+    scrolled: debounce(function(){this.fab = false},500,true),
     move(ev){ this.offset = [this.offset[0] - ev.delta.x, this.offset[1] - ev.delta.y] }
   },
   watch: {
     fab: {
       immediate: true,
-      handler: function(s){ if(!s) return; clearTimeout(this.timeout); this.timeout = setTimeout(vm => vm.fab = false,7500,this) }
+      handler: function(s){ if(s) return; clearTimeout(this.timeout); this.timeout = setTimeout(vm => vm.fab = true,1500,this) }
     }
   }
 }
--- resources/pages/Waiter/Index.vue
+++ resources/pages/Waiter/Index.vue
@@ -1,8 +1,13 @@
 <template>
-  <q-page padding class="q-gutter-y-sm">
+  <q-page padding class="q-gutter-y-sm" v-scroll="scrolled">
     <OrderWaiterSummary />
     <OrderWaiterServable />
     <OrderOngoingTables />
+    <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+      <q-page-sticky v-show="fab" position="bottom-right" :offset="offset">
+        <q-fab icon="add" color="primary" glossy :to="{ name: 'order_new' }" v-touch-pan.prevent.mouse="move" />
+      </q-page-sticky>
+    </transition>
   </q-page>
 </template>
 
@@ -11,9 +16,21 @@ import OrderWaiterSummary from "components/Order/OrderWaiterSummary";
 import OrderWaiterServable from "components/Order/OrderWaiterServable";
 import DigitMetric from "components/Metric/DigitMetric";
 import OrderOngoingTables from "components/Order/OrderOngoingTables";
+import {debounce} from "quasar";
 export default {
   name: 'PageWaiterIndex',
   components: {OrderOngoingTables, DigitMetric, OrderWaiterServable, OrderWaiterSummary},
-  data(){ return { token: new URLSearchParams(document.location.search.substring(1)).get('token') } },
+  data(){ return {
+    token: new URLSearchParams(document.location.search.substring(1)).get('token'),
+    fab: true,  offset: [24,24],
+  } },
+  methods: {
+    scrolled: debounce(function(){ this.fab = false },500,true),
+    move(ev){ this.offset = [this.offset[0] - ev.delta.x, this.offset[1] - ev.delta.y] }
+  },
+  watch: {
+    fab(s){ if(s) return; clearTimeout(this.timeout); this.timeout = setTimeout(vm => vm.fab = true,1500,this) }
+  }
+
 }
 </script>
