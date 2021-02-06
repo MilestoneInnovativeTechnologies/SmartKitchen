b22c2e46ac6a8aedcca2602510a0be7592a2c82c
Firose Hussain <github@firumon.com>
2021-02-06 10:47:40 +0530

    Kitchen bulk item processing implemented

---
+++ resources/components/Kitchen/KitchenTokenDisplayMode.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-btn-group>
+    <q-btn color="secondary" glossy label="Progress Mode" @click="$emit('mode','progress')" />
+    <q-btn color="secondary" glossy label="Token Mode" @click="$emit('mode','token')" />
+    <q-btn color="secondary" glossy label="Both" @click="$emit('mode','both')" />
+  </q-btn-group>
+</template>
+
+<script>
+export default {
+  name: "KitchenTokenDisplayMode"
+}
+</script>
