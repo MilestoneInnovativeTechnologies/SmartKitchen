5f2f9bb4a651292697301c18fb56635558cbe286
Firose Hussain <github@firumon.com>
2021-02-09 17:52:04 +0530

    Component nam corrected

--- resources/pages/Waiter/Menu.vue
+++ resources/pages/Waiter/Menu.vue
@@ -9,7 +9,7 @@ import { mapState } from 'vuex'
 import {h_key} from "assets/helpers";
 import WaiterMenuManage from "components/Menu/WaiterMenuManage";
 export default {
-  name: 'PageWaiterIndex',
+  name: 'PageWaiterMenu',
   components: {WaiterMenuManage},
   computed: {
     ...mapState('menus',{ menus:'data',selected:'s_items' }),
