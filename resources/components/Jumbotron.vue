943b5b8a2143802512d7cfa0716f3a17b85689b2
Firose Hussain <github@firumon.com>
2021-02-12 09:45:36 +0530

    Jumbotron dark theme added

--- resources/components/Jumbotron.vue
+++ resources/components/Jumbotron.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card :flat="flat === undefined ? false : flat" :bordered="bordered === undefined ? false : bordered" :class="cCls">
+  <q-card :flat="flat === undefined ? false : flat" :bordered="bordered === undefined ? false : bordered" :class="cCls" :dark="dark === undefined ? false : dark">
     <q-img basic :src="img" v-if="img">
       <div class="absolute-bottom text-subtitle2 text-center">{{ text }}</div>
     </q-img>
@@ -13,7 +13,7 @@
 <script>
 export default {
   name: "Jumbotron",
-  props: ['text','icon','weight','size','bordered','flat','img','color','font'],
+  props: ['text','icon','weight','size','bordered','flat','img','color','font','dark'],
   computed: {
     cCls(){ return _([]).concat(this.color ? ('bg-'+this.color) : null).concat(this.font ? ('text-'+this.font): null).filter().value() }
   }
