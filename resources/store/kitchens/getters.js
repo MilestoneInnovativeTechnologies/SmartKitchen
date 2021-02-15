7cf33efc22e7874f1fbef744b1df345d82dc5606
Firose Hussain <github@firumon.com>
2021-02-15 17:29:45 +0530

    Stock getters added

--- resources/store/kitchens/getters.js
+++ resources/store/kitchens/getters.js
@@ -1,12 +1,3 @@
-export function kitchen_items ({ assign,items }) {
-  return _(items).pick(assign).flatMap().map('item').value()
-}
-export function kitchen_item_stocks ({ assign,items }) {
-  return _(items).pick(assign).flatMap().mapKeys('item').mapValues('stock').value()
-}
-export function my_kitchen_items ({ items }) {
-  return (id) => _(items[_.toSafeInteger(id)]).flatMap().map(({item}) => _.toSafeInteger(item)).value()
-}
-export function my_kitchen_item_stocks ({ items }) {
-  return (id) => _(items[_.toSafeInteger(id)]).flatMap().mapKeys('item').mapValues('stock').value()
+export function stock({ items }){
+  return _(items).flatMap().groupBy('item').mapValues(kis => _(kis).keyBy('kitchen').mapValues('stock').value()).value()
 }
