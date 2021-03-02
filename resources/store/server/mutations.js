b04c3482f60007c213bfff537571685910569dbd
Firose Hussain <github@firumon.com>
2021-03-02 11:08:18 +0530

    Time interval proper code added

--- resources/store/server/mutations.js
+++ resources/store/server/mutations.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 
 export function sync (state,timeOut) {
-  clearInterval(state.timeOut);
+  clearTimeout(state.timeOut);
   Vue.set(state,'timeOut',timeOut)
   Vue.set(state,'pinged',_.toInteger(new Date().getTime()/1000))
 }
