e61fd109e92b24828b1f07f2a7f736489b7b5240
Firose Hussain <github@firumon.com>
2021-03-02 17:59:19 +0530

    Admin layout footer changed, back button added

--- resources/layouts/AdministratorLayout.vue
+++ resources/layouts/AdministratorLayout.vue
@@ -4,10 +4,9 @@
     <q-header reveal elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
-          <q-btn v-if="$store.state.back" :to="$store.state.back" flat round dense icon="arrow_back_ios" />
+          <q-btn v-show="$route.name !== 'administrator_index'" :to="{ name:'administrator_index' }" flat round dense icon="arrow_back_ios" />
           {{ $store.state.title || admin }}
         </q-toolbar-title>
-<!--        <ManualSync />-->
         <q-btn flat round dense icon="power_settings_new" type="a" :href="logout" />
       </q-toolbar>
     </q-header>
@@ -16,11 +15,11 @@
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="bg-primary text-white" v-show="$store.state.footer">
+<!--    <q-footer elevated class="bg-primary text-white" v-show="$store.state.footer">
       <q-tabs align="left">
         <q-route-tab :to="{ name:'administrator_index' }" label="Home" icon="home"  />
       </q-tabs>
-    </q-footer>
+    </q-footer>-->
 
   </q-layout>
 </template>
