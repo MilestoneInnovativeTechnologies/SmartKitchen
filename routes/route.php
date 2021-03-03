19a5c8e76b6fcdaae4d02189c3c2d31acafd671c
Firose Hussain <github@firumon.com>
2021-03-03 16:02:42 +0530

    Routes corrected

--- routes/route.php
+++ routes/route.php
@@ -5,6 +5,7 @@ use \Milestone\SmartKitchen\Controllers\AuthController;
 use \Milestone\SmartKitchen\Controllers\UserController;
 use \Milestone\SmartKitchen\Controllers\APIController;
 use \Milestone\SmartKitchen\Controllers\AssetController;
+use \Milestone\SmartKitchen\Controllers\MediaController;
 use \Milestone\SmartKitchen\Middlewares\SmartKitchenAction;
 use \Milestone\SmartKitchen\Middlewares\SmartKitchenGuest;
 use \Milestone\SmartKitchen\Middlewares\SmartKitchenAuth;
@@ -56,47 +57,18 @@ Route::group([
 
         });
 
+        Route::group([
+            'prefix' => 'media'
+        ],function(){
+            Route::get('/',function(){ return ''; })->name('media_root');
+            Route::post('remove',[MediaController::class,'remove']);
+            Route::post('upload',[MediaController::class,'upload']);
+
+        });
+
     });
 });
 
-/*
-Route::group([
-    'middleware' => ['api','guest'],
-],function(){
-    Route::view('login','SK::login')->name('login');
-    Route::post('login',[AuthController::class,'login']);
-});
-
-
-Route::group([
-    'middleware' => ['api','auth:api'],
-],function(){
-    Route::get('logout',[UserController::class,'logout'])->name('logout');
-    Route::get('refresh',[AuthController::class,'refresh']);
-    Route::view('pre_home','SK::pre_home')->name('pre_home');
-});
-
-Route::view('/','SK::home')->name('home');
-
-Route::group([
-    'middleware' => ['api','auth:api'],
-    'prefix' => 'api/v1'
-],function(){
-    Route::get('/',function(){ return route('base_url'); })->name('base_url');
-    Route::post('ping',[APIController::class,'ping']);
-    Route::post('me',function(){ return ck(); });
-});
-
-Route::group([
-    'middleware' => ['api','auth:api',SmartKitchenAction::class],
-    'prefix' => 'api/v1'
-],function(){
-    Route::post('{item}/{action}',[APIController::class,'ping']);
-});
-*/
-// Temporary
-
 Route::get('test',function(Request $request){
-    $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vc2svbG9naW4iLCJpYXQiOjE2MDc5MTgxNjAsIm5iZiI6MTYwNzkxODE2MCwianRpIjoieFhBSFVlQXBOcEdoU3R2NSIsInN1YiI6MjEsInBydiI6IjYyNDBiNTJjYmE2ZTI0NzA4MDlmODZmNmVhYWIwOWYyNGY5MzM1NjkifQ.ovu0i1sMQrs27YFMUda8LN3hnh7THOYwcB9-1XlQQuo';
-    dd($request->all(),$request->merge(json_decode(base64_decode(explode(".",$token)[1]),true)),$request->all());
+    return \Illuminate\Support\Str::of('priceList')->ucfirst()->__toString();
 });
