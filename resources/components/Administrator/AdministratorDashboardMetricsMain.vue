829d442652a995fa6f4d457f7877dbb0f8c52646
Firose Hussain <github@firumon.com>
2021-03-06 11:56:21 +0530

    Admin metrics modified

--- resources/components/Administrator/AdministratorDashboardMetricsMain.vue
+++ resources/components/Administrator/AdministratorDashboardMetricsMain.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row q-col-gutter-xs">
-    <div class="col-xs-6 col-sm-3"><DigitMetric :value="customers.length" bg="teal" text="white" title="Total Customers" icon="supervised_user_circle" /></div>
-    <div class="col-xs-6 col-sm-3"><DigitMetric :value="tokens.length" bg="blue" text="white" title="Tokens Today" icon="description" /></div>
-    <div class="col-xs-6 col-sm-3"><DigitMetric :value="payments" bg="lime" text="white" title="Today Payments" icon="account_balance_wallet" /></div>
+    <div class="col-xs-6 col-sm-3"><DigitMetric :value="customers" bg="teal" text="white" title="Total Customers" icon="supervised_user_circle" /></div>
+    <div class="col-xs-6 col-sm-3"><DigitMetric :value="items" bg="blue" text="white" title="Total Items" icon="category" /></div>
+    <div class="col-xs-6 col-sm-3"><DigitMetric :value="seats" bg="lime" text="white" title="Today Seats" icon="event_seat" /></div>
     <div class="col-xs-6 col-sm-3"><DigitMetric :value="least.length" bg="orange" text="white" title="Least Stock" icon="trending_down" /></div>
   </div>
 </template>
@@ -14,9 +14,9 @@ export default {
   name: "AdministratorDashboardMetricsMain",
   components: {DigitMetric},
   computed: {
-    customers(){ return Object.values(this.$store.state.customers.data) },
-    tokens(){ return _.filter(this.$store.state.tokens.data,({ date }) => is_today(date)) },
-    payments(){ return _(this.$store.state.payments.data).filter(({ date }) => is_today(date)).sumBy(({ amount }) => _.toNumber(amount)) },
+    customers(){ return _.size(Object.values(this.$store.state.customers.data)) },
+    items(){ return _.size(Object.values(this.$store.state.items.data)) },
+    seats(){ return _.size(Object.values(this.$store.state.seating.data)) },
     least(){ return _(this.$store.state.kitchens.items).flatMap().filter(({ stock }) => stock < 5).value() },
   }
 }
