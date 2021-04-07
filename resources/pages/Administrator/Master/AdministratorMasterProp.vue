b8c327880f8ddacdd3b18d3081ae3c5d8be768c3
Firose Hussain <github@firumon.com>
2021-04-07 17:43:03 +0530

    Item props implemented

---
+++ resources/pages/Administrator/Master/AdministratorMasterProp.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+    <AdministratorMasterCommon label="name" :items="prop" content="item" create="prop" update="prop" :validate="validate" :filter="filter" :fields="fields" />
+  </q-page>
+</template>
+
+<script>
+
+import { mapState } from "vuex";
+import AdministratorMasterCommon from "components/Administrator/AdministratorMasterCommon";
+
+export default {
+  name: "PageAdministratorMasterGroup",
+  components: {AdministratorMasterCommon},
+  data(){ return {
+    fields: { name:'text' },
+    validate: ['name'],
+    filter: ['name'],
+  } },
+  computed: mapState('items',['prop']),
+}
+</script>
