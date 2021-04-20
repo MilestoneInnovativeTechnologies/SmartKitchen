a64ffcb940864fded2ad32a74daf3edb047adc4e
Firose Hussain <github@firumon.com>
2021-04-20 11:52:56 +0530

    External remote kitchens wont get logged out

--- src/Jobs/ChefLogOut.php
+++ src/Jobs/ChefLogOut.php
@@ -7,7 +7,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Milestone\SmartKitchen\Models\Kitchen;
 use Milestone\SmartKitchen\Models\KitchenStatus;
+use Milestone\SmartKitchen\Models\Remote;
 use Milestone\SmartKitchen\Models\User;
 
 class ChefLogOut
@@ -21,12 +23,14 @@ class ChefLogOut
     }
 
     public function handle() {
-        $chef = $this->chef->id;
-        if(!KitchenStatus::whereJsonContains('Users',strval($chef))->orWhereJsonContains('Users',intval($chef))->exists())
-            return Log::info("Logging out Chef: $chef, No Kitchens");
-        KitchenStatus::whereJsonContains('Users',strval($chef))->orWhereJsonContains('Users',intval($chef))->each(function($ks)use($chef){
+        $chef = $this->chef->id; $rKs = Remote::where('item','kitchen')->pluck('local_id')->toArray();
+        $Orm = KitchenStatus::whereNotIn('kitchen',$rKs)->where(function($query) use($chef){
+            $query->whereJsonContains('users',strval($chef))->orWhereJsonContains('users',intval($chef));
+        });
+        if($Orm->exists()) $Orm->each(function($ks)use($chef){
             $users = $ks->users; $chefs = array_values(array_unique(array_diff($users,[$chef])));
             $ks->users = $chefs; $ks->save(); Log::info("Chef: $chef Logged Out from Kitchen: {$ks->kitchen}");
         });
+        else Log::info("Logging out Chef: $chef, No Kitchens");
     }
 }
