3b9bf6ac85ed90e5c95fe19ed49f4f3e553fd005
Firose Hussain <github@firumon.com>
2021-07-24 11:35:50 +0530

    Detail wide view component created

---
+++ resources/components/ItemDetailWide.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row justify-between" v-bind="$attrs">
+    <div class="text-caption">{{ label }}</div>
+    <div class="text-caption text-bold"><slot></slot></div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ItemDetailWide',
+  props: ['label'],
+}
+</script>
