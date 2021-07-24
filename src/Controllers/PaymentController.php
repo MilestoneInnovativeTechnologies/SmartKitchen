dcfee89bee9ab0e305db76c0dc61b640b156c340
Firose Hussain <github@firumon.com>
2021-07-24 11:31:18 +0530

    Payment cancellation method added

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -3,11 +3,22 @@
 namespace Milestone\SmartKitchen\Controllers;
 
 use Illuminate\Http\Request;
+use Milestone\SmartKitchen\Jobs\UpdateBillProgress;
+use Milestone\SmartKitchen\Models\Payment;
 use Milestone\SmartKitchen\Requests\CreatePaymentRequest;
 
 class PaymentController extends Controller
 {
     public function create(CreatePaymentRequest $createPaymentRequest){
         $createPaymentRequest->store();
+    }
+
+    public function cancel(Request $request){
+        if(!$request->id) return null;
+        $Payment = Payment::find($request->id);
+        $Payment->status = 'Inactive';
+        $Payment->save();
+        UpdateBillProgress::dispatch($Payment->bill);
+        return $Payment;
     }
 }
