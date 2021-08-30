e375992fe061a5f2d50806ed5444dc9f0ed1459b
Firose Hussain <github@firumon.com>
2021-08-30 11:42:43 +0530

    If only 1 menu, it will select automatically

--- resources/components/Menu/MenuSelect.vue
+++ resources/components/Menu/MenuSelect.vue
@@ -1,10 +1,10 @@
 <template>
   <q-card>
-    <q-card-section class="bg-grey-2 row items-center">
+    <q-card-section class="bg-grey-2 row items-center q-py-xs">
       <div class="text-bold">Select Menu</div><q-space />
-      <q-btn round icon="clear" flat class="text-weight-bold" color="red-12" v-show="selected.length" v-close-popup />
+      <q-btn round icon="clear" flat class="text-weight-bold" color="red-12" v-show="selected.length || !menus_count" v-close-popup />
     </q-card-section>
-    <q-list separator bordered>
+    <q-list separator bordered v-if="menus_count">
       <q-item v-for="menu in menus" :key="hKey(menu)" clickable @click="selected = menu" v-ripple>
         <q-item-section>
           <q-item-label>{{ menu.name }}</q-item-label>
@@ -14,6 +14,7 @@
         </q-item-section>
       </q-item>
     </q-list>
+    <q-card-section class="text-red" v-else>It seems there is no any menu created. Ask administrator to create a menu</q-card-section>
   </q-card>
 </template>
 
@@ -28,6 +29,7 @@ export default {
   } },
   computed: {
     ...mapState('menus',{ menus:'data',assigned:'s_items' }),
+    menus_count(){ return _.size(this.menus) },
     selected: {
       get(){ return this.assigned },
       set({ id }){ this.$store.dispatch('menus/toggle',id) }
@@ -38,7 +40,10 @@ export default {
     hKey({ id }){ return h_key('waiter','menu','select',id) },
   },
   created() {
-    if(!this.selected.length && _.size(this.menus) === 1) this.selected = _.first(_.values(this.menus))
+    if(!this.selected.length && this.menus_count === 1) {
+      this.selected = _.first(_.values(this.menus));
+      this.$emit('selected');
+    }
   }
 }
 </script>
