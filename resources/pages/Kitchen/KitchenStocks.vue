171a06e3d80e07d702b60e34305870d5c27f5750
Firose Hussain <github@firumon.com>
2021-09-01 21:26:23 +0530

    Width changed

--- resources/pages/Kitchen/KitchenStocks.vue
+++ resources/pages/Kitchen/KitchenStocks.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="q-mx-auto" style="max-width: 700px; width: 65vw">
+    <div class="q-mx-auto" style="width: 90vw">
       <KitchenStocksDisplay :kitchen="id" class="q-mb-sm" />
     </div>
   </q-page>
@@ -13,10 +13,5 @@ export default {
   name: 'PageKitchenStocks',
   props: ['id'],
   components: {KitchenStocksDisplay},
-  computed: {
-
-  },
-  methods: {
-  }
 }
 </script>
