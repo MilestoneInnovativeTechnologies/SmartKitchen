38b6333641593c0fd42c9f4990931a7abac0837d
Firose Hussain <github@firumon.com>
2021-09-01 19:57:35 +0530

    customer menu brought to title from footer

--- resources/layouts/DeliveryBoyLayout.vue
+++ resources/layouts/DeliveryBoyLayout.vue
@@ -7,8 +7,9 @@
           <q-btn v-if="$store.state.back" :to="$store.state.back" flat round dense icon="arrow_back_ios" />
           {{ $store.state.title || name }}
         </q-toolbar-title>
+        <q-btn flat round dense icon="switch_account" :to="{ name:'delivery_boy_customers' }" />
         <ManualSync />
-        <q-btn flat round dense icon="power_settings_new" type="a" :href="logout" />
+        <Logout />
       </q-toolbar>
     </q-header>
 
@@ -20,9 +21,8 @@
       <q-tabs align="left">
         <q-route-tab :to="{ name:'delivery_boy_index' }" label="Home" icon="home"  />
         <q-route-tab :to="{ name:'orders' }" label="Orders" icon="add_task" />
-        <q-route-tab :to="{ name:'delivery_boy_completed' }" label="Ready" icon="receipt" />
+        <q-route-tab :to="{ name:'delivery_boy_completed' }" label="Billable" icon="receipt" />
         <q-route-tab :to="{ name:'delivery_boy_payments' }" label="Payments" icon="account_balance" />
-        <q-route-tab :to="{ name:'delivery_boy_customers' }" label="Customers" icon="switch_account" />
       </q-tabs>
     </q-footer>
 
@@ -31,9 +31,10 @@
 
 <script>
 import ManualSync from "components/ManualSync";
+import Logout from "components/Logout";
 export default {
   name: 'DeliveryBoyLayout',
-  components: {ManualSync},
+  components: {Logout, ManualSync},
   data () { return { name:_USER.name, logout: LOGOUT } },
 }
 </script>
