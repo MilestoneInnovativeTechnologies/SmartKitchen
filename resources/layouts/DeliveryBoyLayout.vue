c900763492fa932f67a33ed7649a56c236b3c908
Firose Hussain <github@firumon.com>
2021-09-01 21:24:56 +0530

    Billable will show alert and notification for pending bills

--- resources/layouts/DeliveryBoyLayout.vue
+++ resources/layouts/DeliveryBoyLayout.vue
@@ -21,7 +21,7 @@
       <q-tabs align="left">
         <q-route-tab :to="{ name:'delivery_boy_index' }" label="Home" icon="home"  />
         <q-route-tab :to="{ name:'orders' }" label="Orders" icon="add_task" />
-        <q-route-tab :to="{ name:'delivery_boy_completed' }" label="Billable" icon="receipt" />
+        <q-route-tab :to="{ name:'delivery_boy_completed' }" label="Billable" icon="receipt" :alert="billable.length > 0 ? 'red' : false" alert-icon="new_releases" />
         <q-route-tab :to="{ name:'delivery_boy_payments' }" label="Payments" icon="account_balance" />
       </q-tabs>
     </q-footer>
@@ -32,9 +32,19 @@
 <script>
 import ManualSync from "components/ManualSync";
 import Logout from "components/Logout";
+import {attention} from "assets/helpers";
 export default {
   name: 'DeliveryBoyLayout',
   components: {Logout, ManualSync},
   data () { return { name:_USER.name, logout: LOGOUT } },
+  computed: {
+    billable(){ return _(this.$store.state.tokens.data).filter({ type:'Home Delivery',progress:'Processing' }).filter(({ id }) => is_all_completed(this.$store.state.tokens.items[parseInt(id)])).filter(({ id }) => !_.find(this.$store.state.bills.data,['token',parseInt(id)])).value() }
+  },
+  watch: {
+    billable(Nw,Ol){ if(!Ol || Nw.length > Ol.length) { attention(); } },
+  }
+}
+function is_all_completed(items){
+  return items.length && _.every(items,({ progress }) => _.includes(['Cancelled','Completed','Served'],progress))
 }
 </script>
