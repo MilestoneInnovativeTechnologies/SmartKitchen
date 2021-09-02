9a8ff4cd8690bc1938ca70ccaf9adb08afd694b0
Firose Hussain <github@firumon.com>
2021-09-02 13:11:24 +0530

    Bill summary, user added

--- resources/pages/Report/Reports/ReportBillSummary.vue
+++ resources/pages/Report/Reports/ReportBillSummary.vue
@@ -16,8 +16,8 @@ export default {
   computed: {
     range(){ return range(this.$store.state.public.range) },
     head(){ return _.zipObject(['Report','Start Date','End Date'],['Bill Summary',to_format('ddd DD/MMM hh:mm',this.range.from),to_format('ddd DD/MMM hh:mm',this.range.to)]) },
-    table(){ return _(this.bills).filter(({ date,progress }) => progress !== 'Cancelled' && is_between(date,this.range.from,this.range.to)).map(({ date,customer:{ name },payable,paid }) => _.zipObject(['Time','Customer','Payable','Paid','Balance'],[
-      time(date),name,precision(payable,2),precision(paid,2),precision(payable-paid,2)
+    table(){ return _(this.bills).filter(({ date,progress }) => progress !== 'Cancelled' && is_between(date,this.range.from,this.range.to)).map(({ date,payable,paid,user }) => _.zipObject(['Time','User','Payable','Paid','Balance'],[
+      time(date),_.get(user,'name'),precision(payable,2),precision(paid,2),precision(payable-paid,2)
     ])).value() },
     foot(){ return _.zipObject(['Total Payable','Total Paid','Total Balance'],[precision(_.sumBy(this.table,({ Payable }) => _.toNumber(Payable)),2),precision(_.sumBy(this.table,({ Paid }) => _.toNumber(Paid)),2),precision(_.sumBy(this.table,({ Balance }) => _.toNumber(Balance)),2)]) }
   }
