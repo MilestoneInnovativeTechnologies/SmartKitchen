57849aa2e9a808d02f03f05ef70d01495c398682
Firose Hussain <github@firumon.com>
2021-09-04 13:30:28 +0530

    Validation added for seat for dining orders

--- resources/components/Order/OrderNewBasic.vue
+++ resources/components/Order/OrderNewBasic.vue
@@ -3,13 +3,13 @@
     <q-bar class="text-white q-py-sm" style="font-size: 0.8rem" :class="'bg-' + clr"><q-icon name="account_tree" size="xs" left /><span class="text-bold">New Order Details</span><q-space /><q-btn icon="close" flat dense round v-close-popup /></q-bar>
     <q-card-section class="q-gutter-y-xs">
       <q-select v-if="!hide.includes('type')" outlined dense input-debounce="0" v-model="params.type" :options="types" label="Select Type" />
-      <SeatSelectDropDown v-if="!hide.includes('seating') && required.includes('Seating')" outlined dense input-debounce="0" v-model="seat" label="Select Seat" />
+      <SeatSelectDropDown error-message="For Dining Seat Selection is Mandatory" :error="seat_error" hide-bottom-space v-if="!hide.includes('seating') && required.includes('Seating')" outlined dense input-debounce="0" v-model="seat" label="Select Seat" />
       <PriceListSelectDropDown v-if="!hide.includes('price_list') && required.includes('PriceList')" outlined dense input-debounce="0" v-model="params.price_list" label="Select Price List" get="id" />
       <WaiterSelectDropDown v-if="!hide.includes('user') && required.includes('Waiter')" outlined dense input-debounce="0" v-model="params.user" label="Select Waiter" get="id" />
       <OrderCustomer v-if="required.includes('Customer')" v-model="params.customer" get="id" />
     </q-card-section>
     <q-card-actions align="right" class="bg-grey-2">
-      <q-btn icon-right="forward" :color="clr" padding="xs md" size="sm" label="Proceed" @click="$router.push({ name:'order_new',params:{ seat:false,...params } })" />
+      <q-btn icon-right="forward" :color="clr" padding="xs md" size="sm" label="Proceed" @click="proceed" />
     </q-card-actions>
   </q-card>
 </template>
@@ -28,12 +28,13 @@ export default {
   data(){ return {
     params: { type:'Dining',seating:null,customer:null,price_list:null,user:null }, seat: null,
     types: ['Dining','Home Delivery','Take Away'],
-    hide: [],
+    hide: [], validate: false,
   } },
   computed: {
     required(){ return OrderTypeRequiredFields[this.params.type || 'Dining'] },
     clr(){ return this.color || 'cyan' },
     attrs(){ return _.pick(this.$attrs,_.keys(this.params)) },
+    seat_error(){ return this.validate ? (this.params.type === 'Dining' && !this.params.seating) : false },
   },
   methods: {
     init(attrs){ let vm = this; _.forEach(attrs,function(val,key){ if(val !== undefined) vm.hide.push(key); vm.params[key] = val }) },
@@ -44,7 +45,11 @@ export default {
         this.$store.commit('public',{ [state_name]:price_list_id })
       }
       this.params.price_list = this.$store.state.public[state_name];
-    }
+    },
+    proceed(){
+      this.validate = true; if(this.seat_error) return; this.validate = false;
+      this.$router.push({ name:'order_new',params:{ seat:false,...(this.params) } });
+    },
   },
   watch: {
     seat({ id,price_list }){ this.params.seating = id; this.params.price_list = price_list },
