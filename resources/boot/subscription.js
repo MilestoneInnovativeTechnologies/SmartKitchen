9e7c2cc2f393e46ea129264a3d4f79c80a3d12df
Firose Hussain <github@firumon.com>
2021-09-11 18:39:03 +0530

    Subscription bugs rectified

--- resources/boot/subscription.js
+++ resources/boot/subscription.js
@@ -2,12 +2,13 @@ const MD5 = require('js-md5')
 import axios from 'axios'
 
 const GLOBAL_FEATURES = [["GH56E","Yes/No","No"],["GH75F","Yes/No","No"],["JI36A","Detail",null]]
-const KEY = localStorage.getItem('key')
+const KEY = localStorage.getItem('key') || ''
 const CODE = localStorage.getItem('code')
 const SERIAL = localStorage.getItem('serial')
 
+if(!KEY) location.href = LOGIN.replace('login','subscription');
 
-export default async () => {
+export default () => {
   if(SERIAL || typeof _USER !== 'undefined') return;
   axios.post(LOGIN.replace('login','subscription/serial')).then(({ headers }) => localStorage.setItem('serial',headers['sk-serial']) || location.reload())
 }
@@ -18,8 +19,11 @@ let CODE_VALID = true, CODE_INVALID_REASON = '', CODE_INVALID_DETAIL = '', CODE_
 // E00
 if(CODE_VALID && (!KEY || !CODE || !SERIAL)) { CODE_VALID = false; CODE_INVALID_REASON = "Subscription details missing"; CODE_INVALID_DETAIL = "KEY, CODE or SERIAL is not accessible"; CODE_INVALID_ERROR_CODE = 'E00' }
 
+// E01
+if(CODE_VALID && !CODE.indexOf('/')) { CODE_VALID = false; CODE_INVALID_REASON = "Code is corrupted"; CODE_INVALID_DETAIL = "Code have no separator for basic."; CODE_INVALID_ERROR_CODE = 'E01' }
+
 export const KEY_VALID = (KEY.substr(0,2) + MD5(KEY.substr(0,2) + SERIAL + KEY.substr(-4)).toString() + KEY.substr(-4)) === KEY
-const CODE_HAS_BASIC = CODE.split('/').length === 2 && !_.isEmpty(CODE.split('/')[0]) && !_.isEmpty(CODE.split('/')[1])
+const CODE_HAS_BASIC = CODE_VALID && CODE.split('/').length === 2 && !_.isEmpty(CODE.split('/')[0]) && !_.isEmpty(CODE.split('/')[1])
 const CODE_BASIC_JSON_STRING = CODE_HAS_BASIC ? atob(CODE.split("/")[1]) : ""
 let JSON_STRING_VALID = false;
 if(CODE_HAS_BASIC){
@@ -37,7 +41,7 @@ export const CLIENT = SUBSCRIPTION_ARRAY['client'];
 export const BRANCH = SUBSCRIPTION_ARRAY['branch'];
 export const SUBSCRIPTION_NAME = SUBSCRIPTION_ARRAY['sub'];
 
-function KJDU(){ return CODE.split("/")[0] }
+function KJDU(){ return CODE_VALID ? CODE.split("/")[0] : '' }
 function UEPQ(){ return KJDU().substr(34,5) + "" + KJDU().substr(-5); }
 function QWPO(){ return UEPQ() === _.toString(_.toInteger(new Date(SUBSCRIPTION_ARRAY['end']).getTime()/1000)) }
 
@@ -49,14 +53,14 @@ function IAZP(){ return _.toString(UEPQ()).substr(0,5) }
 function LMNP(){ return _.toString(UEPQ()).replace(IAZP(),'') }
 function XAAL(){ return KJDU().substr(0,2) }
 
-// E01
-if(CODE_VALID && !_.toNumber(XAAL())) { CODE_VALID = false; CODE_INVALID_REASON = "CODE is invalid"; CODE_INVALID_DETAIL = "Separator is corrupted"; CODE_INVALID_ERROR_CODE = 'E01' }
+// E02
+if(CODE_VALID && !_.toNumber(XAAL())) { CODE_VALID = false; CODE_INVALID_REASON = "CODE is invalid"; CODE_INVALID_DETAIL = "Separator is corrupted"; CODE_INVALID_ERROR_CODE = 'E02' }
 
 function S0IP(){ return KEY.substr(0,_.toNumber(XAAL())) }
 function K090(){ return KEY.replace(S0IP(),'') }
 
-// E02
-if(CODE_VALID && (!S0IP() || !K090() || KEY !== (S0IP() + K090())))  { CODE_VALID = false; CODE_INVALID_REASON = "CODE and KEY not matches"; CODE_INVALID_DETAIL = "Code is not encoded with provided key"; CODE_INVALID_ERROR_CODE = 'E02' }
+// E03
+if(CODE_VALID && (!S0IP() || !K090() || KEY !== (S0IP() + K090())))  { CODE_VALID = false; CODE_INVALID_REASON = "CODE and KEY not matches"; CODE_INVALID_DETAIL = "Code is not encoded with provided key"; CODE_INVALID_ERROR_CODE = 'E03' }
 
 function AR1P(){ return MD5 }
 function RO06(){ return AR1P()(S0IP()) }
@@ -73,8 +77,8 @@ function TA00(){ return KJDU().indexOf(W93M()) === 0 }
 function TB11(){ return KJDU().split("/")[0].substr(0-KXY3()).indexOf(M39W()) === 0 }
 function CFAD(){ return KJDU().replace(W93M(),'').replace(M39W(),'') }
 
-// E03
-if(CODE_VALID && (!TA00() || !TB11()))  { CODE_VALID = false; CODE_INVALID_REASON = "CODE corrupted"; CODE_INVALID_DETAIL = "Code initial part and final part are not matching"; CODE_INVALID_ERROR_CODE = 'E03' }
+// E04
+if(CODE_VALID && (!TA00() || !TB11()))  { CODE_VALID = false; CODE_INVALID_REASON = "CODE corrupted"; CODE_INVALID_DETAIL = "Code initial part and final part are not matching"; CODE_INVALID_ERROR_CODE = 'E04' }
 
 function N8EJ(key){
   let T = 0;
@@ -88,7 +92,8 @@ function N8EJ(key){
   let F = parseInt(O,16);
   let P = key.substr(key.lastIndexOf("h")+1)
   let G = parseInt(P,16);
-  N.push(key.substring(key.lastIndexOf('g')+1,key.lastIndexOf('h')));
+  let G0 = key.substr(T);
+  N.push(G0.substr(0,G0.length - (P+"").length - 1));
   let J = N.join("");
   let V = [], D = [], E = [], W = '';
   for(let i = 0; i < J.length; i+=(parseInt(C)*2)) V.push(J.substr(i,parseInt(C)*2));
@@ -105,23 +110,23 @@ function N8EJ(key){
   return [atob(A).split("|"),atob(B).split("|")]
 }
 
-// E04
-if(CODE_VALID && !CFAD()) {
-  CODE_VALID = false; CODE_INVALID_REASON = "No Features"; CODE_INVALID_DETAIL = "Features are empty"; CODE_INVALID_ERROR_CODE = 'E04'
-}
-
-function YZB6(){ return N8EJ(CFAD()) }
-
 // E05
-if(CODE_VALID && (!_.isArray(YZB6()) || !_.isArray(YZB6()[0]) || !_.isArray(YZB6()[1]) || YZB6()[0].length !==  YZB6()[1].length)){
-  CODE_VALID = false; CODE_INVALID_REASON = "Features corrupted"; CODE_INVALID_DETAIL = "Features corrupted"; CODE_INVALID_ERROR_CODE = 'E05'
+if(CODE_VALID && !CFAD()) {
+  CODE_VALID = false; CODE_INVALID_REASON = "No Features"; CODE_INVALID_DETAIL = "Features are empty"; CODE_INVALID_ERROR_CODE = 'E05'
 }
 
-function MJ78(){ return _.zipObject(YZB6()[0],YZB6()[1]) }
+const YZB6 = CODE_VALID ? N8EJ(CFAD()) : false
 
 // E06
+if(CODE_VALID && (!_.isArray(YZB6) || !_.isArray(YZB6[0]) || !_.isArray(YZB6[1]) || YZB6[0].length !==  YZB6[1].length)){
+  CODE_VALID = false; CODE_INVALID_REASON = "Features corrupted"; CODE_INVALID_DETAIL = "Features corrupted"; CODE_INVALID_ERROR_CODE = 'E06'
+}
+
+function MJ78(){ return _.zipObject(YZB6[0],YZB6[1]) }
+
+// E07
 if(CODE_VALID && !MJ78()){
-  CODE_VALID = false; CODE_INVALID_REASON = "Features Corrupted"; CODE_INVALID_DETAIL = "Features compile error"; CODE_INVALID_ERROR_CODE = 'E06'
+  CODE_VALID = false; CODE_INVALID_REASON = "Features Corrupted"; CODE_INVALID_DETAIL = "Features compile error"; CODE_INVALID_ERROR_CODE = 'E07'
 }
 
 const FEATURES = {}
@@ -132,9 +137,9 @@ function DF00(){
   return true;
 }
 
-// E07
+// E08
 if(CODE_VALID && !DF00()){
-  CODE_VALID = false; CODE_INVALID_REASON = "Final Stage"; CODE_INVALID_DETAIL = "Final Stage unknown error"; CODE_INVALID_ERROR_CODE = 'E07'
+  CODE_VALID = false; CODE_INVALID_REASON = "Final Stage"; CODE_INVALID_DETAIL = "Final Stage unknown error"; CODE_INVALID_ERROR_CODE = 'E08'
 }
 
 export { CODE_VALID,CODE_INVALID_ERROR_CODE,CODE_INVALID_DETAIL,CODE_INVALID_REASON,FEATURES }
