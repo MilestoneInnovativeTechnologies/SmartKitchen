a93dd43b10cb21ec822a7e7a031bf1c4dfd35046
Firose Hussain <github@firumon.com>
2021-09-11 11:54:47 +0530

    Order type selecting dropdown created

---
+++ resources/components/Order/OrderTypeSelectDropDown.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-select :options="options" v-bind="$attrs" :value="optValue" @input="doEmit" label="Type" />
+</template>
+
+<script>
+import SelectDropDown from "assets/mixins/SelectDropDown";
+import {OrderTypes} from "assets/assets";
+
+export default {
+  name: "OrderTypeSelectDropDown",
+  mixins: [SelectDropDown],
+  props: ['skip'],
+  computed: {
+    options(){ return _.difference(OrderTypes,this.skips) },
+    skips(){ return _.isString(this.skip) ? [this.skip] : this.skip }
+  }
+}
+</script>
