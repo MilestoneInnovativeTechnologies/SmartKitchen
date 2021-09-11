7849a86eaa46e0681df9050bc87834eef0232285
Firose Hussain <github@firumon.com>
2021-09-11 11:53:57 +0530

    Auto time popup on date selection added

--- resources/components/DateTime.vue
+++ resources/components/DateTime.vue
@@ -2,8 +2,8 @@
   <q-input v-model="date" v-bind="$attrs">
     <template v-slot:append>
       <q-icon name="event" class="cursor-pointer">
-        <q-popup-proxy transition-show="scale" transition-hide="scale">
-          <q-date v-model="date" mask="YYYY-MM-DD HH:mm:ss">
+        <q-popup-proxy transition-show="scale" transition-hide="scale" @before-hide="$refs['time_pop'].show()">
+          <q-date v-model="date" mask="YYYY-MM-DD HH:mm:ss" today-btn>
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Ok" color="primary" flat @click="$emit('input',date)" />
             </div>
@@ -11,8 +11,8 @@
         </q-popup-proxy>
       </q-icon>
       <q-icon name="access_time" class="cursor-pointer">
-        <q-popup-proxy transition-show="scale" transition-hide="scale">
-          <q-time v-model="date" mask="YYYY-MM-DD HH:mm:ss" format24h>
+        <q-popup-proxy transition-show="scale" transition-hide="scale" ref="time_pop">
+          <q-time v-model="date" mask="YYYY-MM-DD HH:mm:ss">
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Ok" color="primary" flat @click="$emit('input',date)" />
             </div>
