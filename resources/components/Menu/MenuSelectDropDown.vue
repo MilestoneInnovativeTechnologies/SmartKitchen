89ac19ce1141f0bd8a30842a4ecb0c1506e1cc7d
Firose Hussain <github@firumon.com>
2021-09-11 11:54:26 +0530

    Menu selecting dropdown created

---
+++ resources/components/Menu/MenuSelectDropDown.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-select :options="options" v-bind="$attrs" :value="optValue" @input="doEmit" label="Select Menu" option-label="name" />
+</template>
+
+<script>
+import SelectDropDown from "assets/mixins/SelectDropDown";
+
+export default {
+  name: "MenuSelectDropDown",
+  mixins: [SelectDropDown],
+  computed: {
+    options(){ return _.map(this.$store.state.menus.data) }
+  }
+}
+</script>
