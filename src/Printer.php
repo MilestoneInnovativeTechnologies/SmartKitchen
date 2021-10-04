e9f935517732ec971cb08313876ccb98f569307d
Firose Hussain <github@firumon.com>
2021-10-04 13:49:32 +0530

    Printing image implemented

--- src/Printer.php
+++ src/Printer.php
@@ -4,6 +4,7 @@
 namespace Milestone\SmartKitchen;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use Mike42\Escpos\Printer as P;
 use Mike42\Escpos\CapabilityProfile;
 use Mike42\Escpos\EscposImage;
@@ -158,9 +159,11 @@ class Printer
     }
 
     private function process_image($path,$props){
-        $path = self::exec_template($path, $this->_data);
+        $name = self::exec_template($path, $this->_data);
+        $file = Storage::path(Arr::get(Settings::where('name','_file_' . $name)->first(),'value'));
+        if(!$file) return;
         try {
-            $img = EscposImage::load($path, false);
+            $img = EscposImage::load($file, false);
             $this->process_props($props,"",false);
             $this->Printer->bitImage($img);
         } catch (\Exception $e) {
@@ -247,7 +250,7 @@ class Printer
 
 /*
  * Inorder to have this work, settings should have a value like '//localhost/EPSON' under any name (printer name).. Ex: PRINTER001 This name is referred while calling the class Printer
- * Every settings related to this printer should provide in settings as <printer_name>_<settings name>.. Ex: PRINTER001_width
+ * Every setting related to this printer should provide in settings as <printer_name>_<settings name>.. Ex: PRINTER001_width
  * Required settings are connector -> windows shared printers => FilePrintConnector, windows printer => WindowsPrintConnector, more details @ https://github.com/mike42/escpos-php
  * capability -> more details @ https://github.com/mike42/escpos-php
  * width -> width in no of characters a line can include.. In thermal printer it may line 32,48 etc. Default is 48
@@ -260,8 +263,8 @@ class Printer
  * Each section itself will be another array, having first element as text or template string
  * Second element should be the properties given as string and multiple props separated by | "pipe character"
  * Template string can be
- * Hi Mr [customer.name], how you doing.. You line number is [id] and time [delivery.time]
- * Thee square brackets are removed with value supplied in data array
+ * Hi Mr [customer.name], how you're doing. You line number is [id] and time [delivery.time]
+ * The square brackets are removed with value supplied in data array
  * Supported properties are
  * feed
  * left
@@ -276,12 +279,13 @@ class Printer
  * around -> print on each side of a line. Text should have left and right portion separated by | "pipe character"
  * stack -> print on top bottom. Text should have top and bottom portion separated by | "pipe character". Top text will try to emphasis. Other props like left,center,right,underline are also accepted as props
  * table -> String should be the field details ex: 'items#Details:[quantity]x [item.name]|Narration:[narration]|Customer:[customer.name]|Seat:[seating.name]
- * where the items is the key of data where table data is located
- * each field would be separated by |. Each field should have field name and its value separated by :
- * An optional 3rd argument can be given if there is any size specification. It should be array having each element size of corresponding column. Ex: [2,3,4]
- * An optional 4th argument can be given if there is any column to be aligned right
- * In props, index can be added for having sequence no as output
+ *  where the items are the key of data where table data is located
+ *  each field would be separated by |. Each field should have field name and its value separated by :
+ *  An optional 3rd argument can be given if there is any size specification. It should be array having each element size of corresponding column. Ex: [2,3,4]
+ *  An optional 4th argument can be given if there is any column to be aligned right
+ *  In props, index can be added for having sequence number in each row
  * upi -> this is to print payment upi qr code. text should be like '[id]|[amount]', where ID and amount should be separated by "|". Other props are also supported.
- * Inorder to have this work, settings should contain upi_address and upi_name
+ *  Inorder to have this work, settings should contain upi_address and upi_name
+ * image -> Text should be the name, which were used to upload file in settings > upload. Other props are also supported.
  *
  */
