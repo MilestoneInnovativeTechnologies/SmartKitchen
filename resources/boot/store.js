4a4040438e2d2524498ec5fce8f151a9695d2326
Firose Hussain <github@firumon.com>
2021-10-21 14:08:03 +0530

    Settings global function created

--- resources/boot/store.js
+++ resources/boot/store.js
@@ -8,4 +8,7 @@ export default async ({ store,router }) => {
   }
   global.fetch_records = function(item,ids,key){ return post(item,'records',{ ids:_.isArray(ids) ? ids : [ids], key:key || 'id' }) }
   if(!get(router,['options','routes',0,'meta','online_menu'],false)) _.forEach(store._actions,(action,name) => (name.substr(-4) === 'init') ? store.dispatch(name,null,{ root:true }) : null);
+  global.settings = function(name){
+    return _.get(store.getters['settings/settings'],name)
+  }
 }
