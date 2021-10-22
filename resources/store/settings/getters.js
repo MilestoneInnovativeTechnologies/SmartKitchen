81befdc66008d4599eecc2ad735175e33925ca6c
Firose Hussain <github@firumon.com>
2021-10-22 12:45:48 +0530

    Settings will consider getters also

--- resources/store/settings/getters.js
+++ resources/store/settings/getters.js
@@ -4,3 +4,9 @@ export function settings ({ data }) {
 export function items_per_page(state,getters){
   return _.toInteger(_.get(getters.settings,'items_per_page',_.get(getters.settings,'max_items_per_page',100)))
 }
+export function items_search_fields(state,{ settings }){
+  return function(name){ name = _.snakeCase(name || 'order'); let key = name + '_items_search_fields'
+    let fields = _.get(settings,key,_.get(settings,'items_search_fields','id,name,detail'))
+    return _.split(fields,',')
+  }
+}
