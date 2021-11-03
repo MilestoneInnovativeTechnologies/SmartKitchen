f599cd7b85d115350b8beed2d0fafa34a0393301
Firose Hussain <github@firumon.com>
2021-11-03 11:42:10 +0530

    New features data added

--- resources/assets/features.js
+++ resources/assets/features.js
@@ -1,1 +1,1 @@
-export const GLOBAL_FEATURES = [["GH56E","Yes\/No","No"],["GH75F","Yes\/No","No"],["JI36A","Detail",null],["CC71V","Yes\/No","No"],["DP71V","Detail",null]]
+export const GLOBAL_FEATURES = [["GH56E","Yes\/No","No"],["GH75F","Yes\/No","No"],["JI36A","Detail",null],["CC71V","Yes\/No","No"],["DP71V","Detail",null],["JX99V","Yes\/No","No"],["KK99V","Yes\/No","Yes"]]
