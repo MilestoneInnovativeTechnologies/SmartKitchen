54bcf127c30307f9946e7c346243cae0ce987e91
Firose Hussain <github@firumon.com>
2021-11-03 11:40:36 +0530

    Additional fetching, contents parse as array

--- resources/store/bills/mutations.js
+++ resources/store/bills/mutations.js
@@ -5,6 +5,7 @@ export function add (state,records) {
   _.forEach(records,data => {
     if(!data.id) return ; let key = _.toSafeInteger(data.id);
     if(_.has(state.data,key)) Vue.delete(state.data,key);
+    if(_.isString(data.contents)) { try { data.contents = JSON.parse(data.contents) } catch(e){ data.contents = [] } }
     Vue.set(state.data,key,data)
   })
 }
