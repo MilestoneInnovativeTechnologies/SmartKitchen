08c189c45949e844b008bb4142b57ec808ad0f5e
Firose Hussain <github@firumon.com>
2021-11-29 16:52:59 +0530

    Remote order made effective

--- resources/pages/Token/Tokens.vue
+++ resources/pages/Token/Tokens.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page padding>
-    <KitchenTokens :id="kitchen" :card="false" />
+    <KitchenTokens v-if="kitchen" :id="kitchen.id" :card="false" />
     <transition-group appear enter-active-class="animated zoomIn" leave-active-class="animated zoomOut">
       <q-page-sticky :key="'ktp-rs-b'" position="bottom-left" :offset="[18, 18]" class="lt-md" v-show="!reset"><q-btn fab icon="undo" color="negative" padding="sm" @click="reset = true" /></q-page-sticky>
-      <q-page-sticky :key="'ktp-aoo-b'" position="bottom-right" :offset="[18, 18]" class="lt-md" v-show="!remote_order"><q-btn fab icon="undo" color="secondary" padding="sm" class="flip-horizontal" @click="remote_order = true" /></q-page-sticky>
+      <q-page-sticky :key="'ktp-aoo-b'" position="bottom-right" :offset="[18, 18]" class="lt-md" v-show="!offline_order" v-if="cloud"><q-btn fab icon="undo" color="secondary" padding="sm" class="flip-horizontal" @click="offline_order = true" /></q-page-sticky>
     </transition-group>
-    <q-dialog persistent :value="remote_order" @before-hide="remote_order = false">
-      <AddRemoteOrder style="width: 22rem" :kitchen="kitchen" @imported="remote_order = false" />
+    <q-dialog persistent :value="offline_order" @before-hide="offline_order = false" v-if="cloud">
+      <AddRemoteOrder style="width: 22rem" :kitchen="kitchen.id" @imported="offline_order = false" />
     </q-dialog>
   </q-page>
 </template>
@@ -18,10 +18,11 @@ export default {
   name: 'PageTokens',
   components: {AddRemoteOrder, KitchenTokens},
   data(){ return {
-    remote_order: false,
+    offline_order: false,
   } },
   computed: {
-    kitchen(){ return _.toInteger(this.$store.state.public.kitchen) },
+    kitchen(){ return _.get(this.$store.state.kitchens.data,_.toInteger(this.$store.state.public.kitchen),null) },
+    cloud(){ return _.get(this.kitchen,'cloud') === 'Yes' },
     reset: {
       get(){ return !!_.get(this.$store.state,['public','reset'],false) },
       set(reset){ this.$store.commit('public',{ reset: !!reset }) }
