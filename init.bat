555f537661a5089e917ae8e022387b51b2536939
Firose Hussain <github@firumon.com>
2022-01-04 18:09:06 +0530

    Development friendly updates

--- init.bat
+++ init.bat
@@ -3,3 +3,8 @@ mklink /H .styleci.yml milestone\smartkitchen\root\.styleci.yml
 mklink /H babel.config.js milestone\smartkitchen\root\babel.config.js
 mklink /H package.json milestone\smartkitchen\root\package.json
 mklink /H quasar.conf.js milestone\smartkitchen\root\quasar.conf.js
+mklink /H firebase.json milestone\smartkitchen\root\firebase.json
+mklink /H .firebaserc milestone\smartkitchen\root\.firebaserc
+mklink /H firestore.indexes.json milestone\smartkitchen\root\firestore.indexes.json
+mklink /H firestore.rules milestone\smartkitchen\root\firestore.rules
+mklink /H storage.rules milestone\smartkitchen\root\storage.rules
