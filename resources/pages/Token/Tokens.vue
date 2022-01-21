bf7fed74a85c1cc8909fcebf2661a85d093a5223
Firose Hussain <github@firumon.com>
2022-01-21 16:55:12 +0530

    Offline add shows on every screen sizes

--- resources/pages/Token/Tokens.vue
+++ resources/pages/Token/Tokens.vue
@@ -3,7 +3,7 @@
     <KitchenTokens v-if="kitchen" :id="kitchen.id" :card="false" />
     <transition-group appear enter-active-class="animated zoomIn" leave-active-class="animated zoomOut">
       <q-page-sticky :key="'ktp-rs-b'" position="bottom-left" :offset="[18, 18]" class="lt-md" v-show="!reset"><q-btn fab icon="undo" color="negative" padding="sm" @click="reset = true" /></q-page-sticky>
-      <q-page-sticky :key="'ktp-aoo-b'" position="bottom-right" :offset="[18, 18]" class="lt-md" v-show="!offline_order" v-if="cloud"><q-btn fab icon="undo" color="secondary" padding="sm" class="flip-horizontal" @click="offline_order = true" /></q-page-sticky>
+      <q-page-sticky :key="'ktp-aoo-b'" position="bottom-right" :offset="[18, 18]" v-show="!offline_order" v-if="cloud"><q-btn fab icon="undo" color="secondary" padding="sm" class="flip-horizontal" @click="offline_order = true" /></q-page-sticky>
     </transition-group>
     <q-dialog persistent :value="offline_order" @before-hide="offline_order = false" v-if="cloud">
       <AddRemoteOrder style="width: 22rem" :kitchen="kitchen.id" @imported="offline_order = false" />
