5015e09f56ac3e45b87e02ad0ce8f9bb52d55af0
Firose Hussain <github@firumon.com>
2022-04-23 13:18:40 +0530

    Dashboard card, Non dining orders - color added

--- resources/components/Receptionist/ReceptionistNonDiningTokens.vue
+++ resources/components/Receptionist/ReceptionistNonDiningTokens.vue
@@ -1,6 +1,6 @@
 <template>
   <q-card v-if="oTokens.length">
-    <q-card-section class="bg-grey-2 row justify-between text-bold"><div>Non Dining Orders</div><div>{{ oTokens.length }}</div></q-card-section>
+    <q-card-section class="row justify-between text-bold text-white" :class="'bg-' + clr"><div>Non Dining Orders</div><div>{{ oTokens.length }}</div></q-card-section>
     <q-card-section class="q-pa-xs">
       <Masonry :items="oTokens" width="260">
         <template #item="token">
@@ -21,12 +21,14 @@ export default {
   name: "ReceptionistNonDiningTokens",
   components: {Masonry, OrderSummaryReceptionistOrder},
   mixins: [Tokens],
+  props: ['color'],
   data(){ return {
     activeProgress: ['New','Processing','Completed','Billed','Pending'],
     displayTypes: ['Take Away','Sale','Other'],
   } },
   computed: {
-    oTokens(){ return _(this.tokens).filter(({ type,progress }) => this.displayTypes.includes(type) && this.activeProgress.includes(progress)).value() }
+    oTokens(){ return _(this.tokens).filter(({ type,progress }) => this.displayTypes.includes(type) && this.activeProgress.includes(progress)).value() },
+    clr(){ return this.color || 'teal' }
   },
   methods: {
     hKey({ id }){ return h_key('receptionist','non','dining','token',id) },
