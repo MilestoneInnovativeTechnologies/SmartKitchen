c0e5d744c13258a271b94108d65f7aa19cec69ac
Firose Hussain <github@firumon.com>
2022-05-18 20:09:59 +0530

    Billable only

--- resources/pages/DeliveryBoy/DeliveryBoyCompleted.vue
+++ resources/pages/DeliveryBoy/DeliveryBoyCompleted.vue
@@ -1,15 +1,15 @@
 <template>
   <q-page padding>
     <q-list>
-      <q-item-label header class="text-h6">Own</q-item-label>
-      <q-item-label caption v-if="!own.length" class="q-pl-lg">No Own Tokens</q-item-label>
-      <TokenDetailDeliveryBoyExpansion v-for="token in own" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="own" color="positive" popup :label="token.customer.name" :caption="time(token.date)" />
-      <q-item-label header class="text-h6">Orphan</q-item-label>
-      <q-item-label caption v-if="!orphan.length" class="q-pl-lg">No Orphan Tokens</q-item-label>
-      <TokenDetailDeliveryBoyExpansion v-for="token in orphan" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="orphan" color="accent" popup :label="token.customer.name" :caption="pa(token)" />
-      <q-item-label header class="text-h6">Other</q-item-label>
-      <q-item-label caption v-if="!other.length" class="q-pl-lg">No Others Tokens</q-item-label>
-      <TokenDetailDeliveryBoyExpansion v-for="token in other" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="other" color="secondary" popup :label="np(token)" :caption="wa(token)" />
+<!--      <q-item-label header class="text-h6">Own</q-item-label>-->
+<!--      <q-item-label caption v-if="!own.length" class="q-pl-lg">No Own Tokens</q-item-label>-->
+      <TokenDetailDeliveryBoyExpansion v-for="token in Tokens" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="own" color="positive" popup :label="token.customer.name" :caption="time(token.date)" />
+<!--      <q-item-label header class="text-h6">Orphan</q-item-label>-->
+<!--      <q-item-label caption v-if="!orphan.length" class="q-pl-lg">No Orphan Tokens</q-item-label>-->
+<!--      <TokenDetailDeliveryBoyExpansion v-for="token in orphan" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="orphan" color="accent" popup :label="token.customer.name" :caption="pa(token)" />-->
+<!--      <q-item-label header class="text-h6">Other</q-item-label>-->
+<!--      <q-item-label caption v-if="!other.length" class="q-pl-lg">No Others Tokens</q-item-label>-->
+<!--      <TokenDetailDeliveryBoyExpansion v-for="token in other" :key="'dbc-own-' + token.id" :token="token" @deliver="deliver(token)" @bill="bill(token)" group="other" color="secondary" popup :label="np(token)" :caption="wa(token)" />-->
     </q-list>
     <q-dialog persistent v-model="bill_mode" @before-hide="bill_mode = false"><BillGenerateCard :token="selected" :style="popup_width()" v-if="selected" @generated="bill_mode = false" /></q-dialog>
     <q-dialog persistent v-model="deliver_mode" @before-hide="deliver_mode = false"><DeliveryBoyPaymentCard :token="selected" :style="popup_width()" v-if="selected" @paid="deliver_mode = false" /></q-dialog>
@@ -36,19 +36,18 @@ export default {
     selected: null,
   } },
   computed: {
-    hide_others(){ return settings_boolean(settings('delivery_boy_hide_others_billed')) === true },
-    hide_tokens(){ return this.hide_others ? _(this.bills).filter(bill => ['Pending','Partial'].includes(bill.progress)).filter(bill => do_hide(bill,this.me)).map(bill => _.toInteger(bill.token.id)).value() : [] },
+    billed_tokens(){ return this.$store.getters['tokens/billed'] },
     Tokens(){
       return _(this.tokens)
-        .filter(['type','Home Delivery'])
+        .filter(token => token.type === 'Home Delivery' && token.progress !== 'Cancelled')
         .filter(is_all_completed)
+        .filter(token => !_.includes(this.billed_tokens,token.id))
         .map(token => token.customer ? token : Object.assign({},token,{ customer:NoCustomer }))
-        .filter(token => this.hide_others ? !_.includes(this.hide_tokens,token.id) : true)
         .value()
     },
-    own(){ return _.filter(this.Tokens,['user',this.me]) },
-    orphan(){ return _.filter(this.Tokens,({ user }) => _.isNull(user)) },
-    other(){ return _.filter(this.Tokens,({ user }) => !_.isNull(user) && user !== this.me) },
+    // own(){ return _.filter(this.Tokens,['user',this.me]) },
+    // orphan(){ return _.filter(this.Tokens,({ user }) => _.isNull(user)) },
+    // other(){ return _.filter(this.Tokens,({ user }) => !_.isNull(user) && user !== this.me) },
   },
   methods: {
     time, popup_width,
@@ -60,10 +59,5 @@ export default {
   }
 }
 
-function is_all_completed({ items }){
-  return items.length && _.every(items,({ progress }) => _.includes(['Cancelled','Completed'],progress))
-}
-function do_hide({ user },id){
-  return user && user.role === 'Delivery Boy' && user.id !== id
-}
+function is_all_completed({ items }){ return items.length && _.every(items,({ progress }) => _.includes(['Cancelled','Completed','Served'],progress)) }
 </script>
