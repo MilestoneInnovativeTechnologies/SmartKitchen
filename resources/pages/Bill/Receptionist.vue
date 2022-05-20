7a9f363bf54786d0df2b68a7067b7660ea6b1ecf
Firose Hussain <github@firumon.com>
2022-05-20 17:56:49 +0530

    Bills pagination added

--- resources/pages/Bill/Receptionist.vue
+++ resources/pages/Bill/Receptionist.vue
@@ -1,12 +1,13 @@
 <template>
   <q-page padding>
     <BillFilter class="q-mb-sm" :tokens="FTokens" v-model="Tokens" />
-    <Masonry :width="300" :items="Tokens">
+    <Masonry :width="300" :items="showing">
       <template #item="Token">
         <TokenBillGenerate v-if="Token.progress === 'Completed'" :token="Token" />
-        <BillMakePayment v-else :bill="Bills[Token.id]" />
+        <BillMakePayment v-else :bill="token_bill[Token.id]" />
       </template>
     </Masonry>
+    <div class="q-pa-lg flex flex-center" v-show="more"><q-pagination v-model="page" color="primary" :min="1" :max="max" :max-pages="7" :ellipses="true" :boundary-numbers="true" /></div>
   </q-page>
 </template>
 
@@ -23,11 +24,14 @@ export default {
   mixins: [Bills],
   data(){ return {
     filter_progress: ['Completed','Pending','Partial'],
-    Tokens: null,
+    Tokens: null, page: 1,
   } },
   computed: {
-    Bills(){ return _(this.bills).filter(({ token,progress }) => token && this.filter_progress.includes(progress)).keyBy(({ token }) => token.id).value() },
+    ipp(){ return this.$store.getters['settings/items_per_page'] },
+    max(){ return this.Tokens ? _.ceil(this.Tokens.length/this.ipp) : 1 },
     FTokens(){ return _.filter(this.tokens,({ id,progress }) => this.filter_progress.includes(progress) || _.has(this.Bills,id)) },
+    showing(){ return _.slice(this.Tokens,(this.page-1) * this.ipp,this.page * this.ipp) },
+    more(){ return this.Tokens && (this.Tokens.length > this.showing.length) }
   }
 }
 </script>
--- resources/pages/Bill/Waiter.vue
+++ resources/pages/Bill/Waiter.vue
@@ -4,9 +4,10 @@
     <Masonry :width="300" :items="Tokens">
       <template #item="Token">
         <TokenBillGenerate v-if="Token.progress === 'Completed'" :token="Token" />
-        <BillMakePayment v-else :bill="Bills[Token.id]" />
+        <BillMakePayment v-else :bill="token_bill[Token.id]" />
       </template>
     </Masonry>
+    <div class="q-pa-lg flex flex-center" v-show="more"><q-pagination v-model="page" color="primary" :min="1" :max="max" :max-pages="7" :ellipses="true" :boundary-numbers="true" /></div>
   </q-page>
 </template>
 
@@ -23,14 +24,18 @@ export default {
   components: {BillFilter, Masonry, BillMakePayment, TokenBillGenerate},
   mixins: [Bills],
   data(){ return {
-    Tokens: null
+    Tokens: null, page: 1
   } },
   computed: {
+    ipp(){ return this.$store.getters['settings/items_per_page'] },
+    max(){ return this.Tokens ? _.ceil(this.Tokens.length/this.ipp) : 1 },
     me(){ return _.get(this.$route,['meta','me','id']) },
     filter_progress(){ return _.concat(((settings_boolean(settings('waiter_generate_bills')) !== false) ? ['Completed'] : []),((settings_boolean(settings('waiter_make_payments')) !== false) ? ['Pending','Partial'] : [])) },
     FTokens(){ return _.filter(this.tokens,({ progress,type,user,waiter }) => type === 'Dining' && this.filter_progress.includes(progress) && _.includes([user,_.get(waiter,'id')],this.me)) },
-    token_ids(){ return _.map(this.Tokens,'id') },
-    Bills(){ return _(this.bills).filter(({ token }) => token && this.token_ids.includes(token.id) && token.progress !== 'Completed').keyBy(({ token }) => token.id).value() }
+    // token_ids(){ return _.map(this.Tokens,'id') },
+    // Bills(){ return _(this.bills).filter(({ token }) => token && this.token_ids.includes(token.id) && token.progress !== 'Completed').keyBy(({ token }) => token.id).value() },
+    showing(){ return _.slice(this.Tokens,(this.page-1) * this.ipp,this.page * this.ipp) },
+    more(){ return this.Tokens && (this.Tokens.length > this.showing.length) }
   }
 }
 </script>
