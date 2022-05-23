01eea1980ce987360ffe190a202a24f4bb94847f
Firose Hussain <github@firumon.com>
2022-05-23 18:21:29 +0530

    Generate bill card - style modified

--- resources/components/Bill/TokenBillGenerate.vue
+++ resources/components/Bill/TokenBillGenerate.vue
@@ -3,26 +3,27 @@
     <q-bar class="bg-secondary text-white q-py-lg">
       <q-list class="q-my-xl full-width">
         <q-item style="font-size: 0.75rem" class="q-px-none items-center">
-          <q-item-section side class="text-white">{{ token.id }}</q-item-section>
-          <q-item-section side class="text-white">{{ token.type }}</q-item-section>
-          <q-item-section>{{ token.seating ? token.seating.name : date(token.date) }}</q-item-section>
-          <q-item-section side v-if="token.customer" class="text-white">{{ token.customer.name }}</q-item-section>
-<!--          <q-item-section side><q-btn icon="close" color="white" rounded flat dense v-close-popup class="q-mt-xs" /></q-item-section>-->
+          <q-item-section side><q-icon name="receipt_long" size="sm" color="white" /></q-item-section>
+          <q-item-section class="text-white">
+            <q-item-label class="q-ml-sm text-bold">{{ token.id }}, {{ token.type }}, {{ token.seating ? token.seating.name : date(token.date) }}, {{ token.customer ? token.customer.name : '' }}</q-item-label>
+          </q-item-section>
+          <q-item-section side v-if="close"><q-btn icon="close" color="white" rounded flat dense v-close-popup class="q-mt-xs text-bold" /></q-item-section>
         </q-item>
       </q-list>
     </q-bar>
     <q-list separator style="font-size: 0.75rem">
       <q-item v-for="(item,idx) in items" :key="'tgb-t-'+token.id+'-ti-'+item.id+'-idx-'+idx">
+        <q-item-section side class="text-black">{{ idx+1 }}</q-item-section>
         <q-item-section>
-          <q-item-label caption>{{ item.quantity }}x {{ precision(item.price) }}</q-item-label>
-          <q-item-label>{{ item.item.name }}</q-item-label>
+          <q-item-label caption>Price: {{ precision(item.price) }}</q-item-label>
+          <q-item-label><span class="text-bold">{{ item.quantity }}x</span> {{ item.item.name }}</q-item-label>
         </q-item-section>
         <q-item-section side class="text-bold">{{ precision(item.price * item.quantity) }}</q-item-section>
       </q-item>
       <q-item>
         <q-item-section>
           <q-item-label caption>{{ items.length }}x Items, {{ quantities }}x Quantities</q-item-label>
-          <q-item-label>Total</q-item-label>
+          <q-item-label class="text-bold">Total</q-item-label>
         </q-item-section>
         <q-item-section side class="text-bold" style="font-size: 1rem">{{ precision(total) }}</q-item-section>
       </q-item>
@@ -38,7 +39,7 @@
         <q-item-label caption class="text-capitalize">Payable</q-item-label>
       </div>
       <div class="text-center col">
-        <q-btn label="Generate Bill" color="secondary" class="q-px-md" @click="generate" :disabled="!customer || items.length === 0" />
+        <q-btn label="BILL" color="secondary" class="q-px-md" @click="generate" :disabled="!customer || items.length === 0" />
       </div>
     </q-card-section>
     <q-inner-loading :showing="loading"><q-spinner-facebook color="secondary" size="2em" /></q-inner-loading>
@@ -55,7 +56,7 @@ import TaxNatureSelectDropDown from "components/Tax/TaxNatureSelectDropDown";
 export default {
   name: "TokenBillGenerate",
   components: {TaxNatureSelectDropDown, OrderCustomer, UserSelectDropDown},
-  props: ['token'],
+  props: ['token','close'],
   data(){ return {
     loading: false, discount: 0, tax: null,
     create_customer: null
@@ -90,7 +91,7 @@ export default {
     generate(){
       this.loading = true; if(!this.customer || !this.items || _.size(this.items) === 0) return alert('Select a customer' + ((this.loading = false) || ''));
       let params = { token:this.token.id, customer:this.customer, discount:this.discount,nature:this.tax }
-      post('bill','create',params).then(() => this.$store.dispatch('server/ping',null,{ root:true })).catch().finally(() => this.loading = false)
+      post('bill','create',params).then(() => { this.$emit('generated'); this.loading = false }).catch().finally(() => this.$store.dispatch('server/ping',null,{ root:true }))
     },
   },
 }
