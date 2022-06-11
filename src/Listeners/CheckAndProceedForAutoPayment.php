6ad89322e9573e9348955153a129fe3ca6c0fa98
Firose Hussain <github@firumon.com>
2022-06-11 11:13:16 +0530

    Auto-pay won't pay automatically when payment cancelled

--- src/Listeners/CheckAndProceedForAutoPayment.php
+++ src/Listeners/CheckAndProceedForAutoPayment.php
@@ -17,6 +17,8 @@ class CheckAndProceedForAutoPayment
     public function handle($event)
     {
         $bill_id = $event->bill_id; $user_id = $event->user_id;
+        $request_segments = array_slice(request()->segments(),-2);
+        if(in_array('payment',$request_segments) && in_array('cancel',$request_segments)) return ;
         AutoPayment::dispatch($bill_id,$user_id);
     }
 
