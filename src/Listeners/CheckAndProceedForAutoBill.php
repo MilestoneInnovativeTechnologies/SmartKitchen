9d6d6c54cdbb03394af495a59f3464effbef57dd
Firose Hussain <github@firumon.com>
2022-06-11 11:12:42 +0530

    Auto bill won't bill automatically when bill cancelled

--- src/Listeners/CheckAndProceedForAutoBill.php
+++ src/Listeners/CheckAndProceedForAutoBill.php
@@ -17,6 +17,8 @@ class CheckAndProceedForAutoBill
     public function handle($event)
     {
         if(request()->hasAny(['advance_amount','advance_type'])) return ;
+        $request_segments = array_slice(request()->segments(),-2);
+        if(in_array('bill',$request_segments) && in_array('cancel',$request_segments)) return ;
         $token_id = $event->token_id; $user_id = $event->user;
         AutoBillCompletedOrder::dispatch($token_id,$user_id);
     }
